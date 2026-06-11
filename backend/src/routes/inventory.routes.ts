2fd695d1a2df7d2a682581a5baab96bc82c62446
Amiir25 <amirsadik636@gmail.com>
2026-06-11 07:16:39 +0300

    Use one common middleware for all routes

--- backend/src/routes/inventory.routes.ts
+++ backend/src/routes/inventory.routes.ts
@@ -4,39 +4,42 @@ import { authMiddleware } from "../middleware/auth.middleware.ts";
 
 const router = Router();
 
+// Common middleware
+router.use(authMiddleware);
+
 /**
  * @route   POST inventory/create-item
  * @desc    Create a new inventory item
  * @access  Protected
  */
-router.post('/create-item', authMiddleware, inventoryController.createItem);
+router.post('/create-item', inventoryController.createItem);
 
 /**
  * @route   GET inventory/get-all-items
  * @desc    Retrieve all items in a restaurant
  * @access  Protected
  */
-router.get('/get-all-items', authMiddleware, inventoryController.getItems);
+router.get('/get-all-items', inventoryController.getItems);
 
 /**
  * @route   GET inventory/get-item
  * @desc    Retrieve item by id in a restaurant
  * @access  Protected
  */
-router.get('/get-item/:itemId', authMiddleware, inventoryController.getItemById);
+router.get('/get-item/:itemId', inventoryController.getItemById);
 
 /**
  * @route   PUT inventory/update-item
  * @desc    Update item
  * @access  Protected
  */
-router.put('/update-item/:itemId', authMiddleware, inventoryController.updateItem);
+router.put('/update-item/:itemId', inventoryController.updateItem);
 
 /**
  * @route   DELETE inventory/delete-item
  * @desc    Delete item
  * @access  Protected
  */
-router.delete('/delete-item/:itemId', authMiddleware, inventoryController.deleteItem);
+router.delete('/delete-item/:itemId', inventoryController.deleteItem);
 
 export default router
